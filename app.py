0b23833589d18c5016fe4984524b73fe2d477082
Machi <37982193+veikedann@users.noreply.github.com>
2025-05-09 16:52:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,4 @@ def convert():
         )
         
     except Exception as e:
-        return {'error': str(e)}, 500
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000) 
+        return {'error': str(e)}, 500 
